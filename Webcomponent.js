37cb59ec98d02cf7317463e4bdd693349716f40d
hjsaliski <102257486+hjsaliski@users.noreply.github.com>
2023-07-21 12:39:47 -0600

    Update Webcomponent.js

--- Webcomponent.js
+++ Webcomponent.js
@@ -1,7 +1,7 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML =
-    `<button type="button" id="myBtn">Helper Button</button>` ;   
+    `<button type="button" id="">Helper Button</button>` ;   
    
     class PerformanceHelp extends HTMLElement {
         constructor() {
